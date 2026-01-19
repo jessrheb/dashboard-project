62ecdcd56f0b448363df888de1c4abf14b8ba739
Jessicah Rhebeccah <jessicah.rhebeccah@gmail.com>
2026-01-19 19:01:41 -0300

    Partially filled the accounts page

--- src/material-module.ts
+++ src/material-module.ts
@@ -5,25 +5,31 @@ import { MatToolbar } from '@angular/material/toolbar';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
 import { MatNavList } from '@angular/material/list';
-import { MatIconButton } from '@angular/material/button';
+import { MatButtonModule, MatIconButton } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   imports: [
-    MatSidenavModule,
+    MatButtonModule,
+
+    MatCardModule,
     MatDivider,
     MatIconButton,
     MatIconModule,
     MatMenuModule,
     MatNavList,
+    MatSidenavModule,
     MatToolbar,
   ],
   exports: [
-    MatSidenavModule,
+    MatButtonModule,
+    MatCardModule,
     MatDivider,
     MatIconButton,
     MatIconModule,
     MatMenuModule,
     MatNavList,
+    MatSidenavModule,
     MatToolbar,
   ],
 })
